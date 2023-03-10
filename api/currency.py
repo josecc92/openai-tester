88890312e4eda49d87ae4746938a01f9805c3315
josecc92 <51939862+josecc92@users.noreply.github.com>
2023-03-10 10:59:19 +0800

    Update currency.py

--- api/currency.py
+++ api/currency.py
@@ -10,10 +10,10 @@ class Currency:
     url = 'https://rate.bot.com.tw/xrt?Lang=zh-TW'
     res = requests.get(url)
     soup = BeautifulSoup(res.text, 'html.parser')
-
     div_element = soup.find('div', text=re.compile(currency_index))
+    
     if div_element is None:
         return '無匯率' # 或其他預設值，例如 0
-
-    value = div_element.find_next('td', {'data-table': '本行現金賣出'}).text.strip()
+    value = f"{div_element.text.strip()}匯率: {div_element.find_next('td', {'data-table': '本行現金賣出'}).text.strip()}"
+    print(value)
     return value
