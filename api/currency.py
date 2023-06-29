35fa7152983adfa3c3450f97ac266ec33b2e9bf8
josecc92 <51939862+josecc92@users.noreply.github.com>
2023-06-29 10:48:46 +0800

    Update currency.py

--- api/currency.py
+++ api/currency.py
@@ -16,3 +16,15 @@ class Currency:
             value = f"{time.text} {div_element.text.strip()} 匯率: {div_element.find_next('td', {'data-table': '本行現金賣出'}).text.strip()}"
             return value
     return f"查無{currency_index}匯率"
+
+def get_currency_spot(self, currency_index):
+    url = 'https://rate.bot.com.tw/xrt?Lang=zh-TW'
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    time = soup.find('span',{'class':'time'})
+    div_elements = soup.find_all('div', {'class': 'visible-phone print_hide'})
+    for div_element in div_elements:
+        if currency_index in div_element.text:
+            value = f"{time.text} {div_element.text.strip()} 匯率: {div_element.find_next('td', {'data-table': '本行即期賣出'}).text.strip()}"
+            return value
+    return f"查無{currency_index}即期匯率"
