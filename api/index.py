78acac5a064c9e73ed7a192d1e9b692265242634
josecc92 <51939862+josecc92@users.noreply.github.com>
2024-06-13 12:40:50 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,17 +4,13 @@ from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from api.chatgpt import ChatGPT
 from api.currency import Currency
-from api.ASACalculator import ASACalculator
-
 import os
-
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
 app = Flask(__name__)
 chatgpt = ChatGPT()
 currency = Currency()
-
 def is_float(value):
     try:
         float_value = float(value)
@@ -26,7 +22,6 @@ def is_float(value):
 @app.route('/')
 def home():
     return 'Hello, World!0613'
-
 @app.route("/qrScan")
 def qrScan():
     try:
@@ -34,7 +29,6 @@ def qrScan():
     except Exception as e:
         return f"發生錯誤: {str(e)}"
     return
-
 @app.route("/webhook", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -48,8 +42,6 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
-
-
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     global working_status
@@ -63,24 +55,34 @@ def handle_message(event):
             
         elif event.message.text.lower().startswith("denni$"):
             msg = "你問的好，DENNI$是一位傳奇般的富豪，他的財富程度超越了大多數人的想像。他的財富來源非常多元化，涵蓋了房地產、科技、金融和創業等領域。\n\nDENNI$擁有一個私人島嶼，這座島嶼被他打造成一個真正的天堂。島上有一座宏偉的別墅，擁有無敵海景和無邊際游泳池。他的別墅內設施一應俱全，包括私人電影院、保齡球場、溫泉浴場和網球場。"
-
         elif event.message.text.lower().startswith("asmiles"):
             user_message = event.message.text.strip().lower()
             # 切分訊息為單詞列表
             words = user_message.split()
-            
-            if (len(words) == 4 and words[0] == "asmiles" and is_float(words[1]) and is_float(words[2]))or (len(words) == 5 and words[0] == "asmiles" and is_float(words[1]) and is_float(words[2]) and is_float(words[4])):
+
+            if len(words) == 4 and words[0] == "asmiles" and is_float(words[1]) and is_float(words[2]):
                 if words[3].lower().strip() == "cash":
-                    ASACalculator.get_asa_mile_unit_price(words[2], float(words[1]), Currency.CASH_RATE.strip())
+                    msg = f"已接收到購買里程數：{words[1]}，獲得的里程百分比：{words[3]}%，使用現金。"
                 elif words[3].lower().strip() == "spot":
-                    ASACalculator.get_asa_mile_unit_price(words[2], float(words[1]), Currency.SPOT_RATE.strip())
+                    msg = f"已接收到購買里程數：{words[1]}，獲得的里程百分比：{words[3]}%，即期交易。"
                 else:
-                    msg = "指令不正確，使用以下格式\nasmiles <購買里程數> <獲得的里程百分比> <cash 或 spot> <來回機票所用里程:非必要>"
+                    msg = "指令不正確，使用以下格式\nasmiles <購買里程數> <獲得的里程百分比> <cash 或 spot>"
             else:
-                msg = "指令不正確，使用以下格式\nasmiles <購買里程數> <獲得的里程百分比> <cash 或 spot> <來回機票所用里程:非必要>"
+                msg = "指令不正確，使用以下格式\nasmiles <購買里程數> <獲得的里程百分比> <cash 或 spot>"
             # 將 words 列表的內容顯示為字串
             words_str = ' '.join(words)
             msg += f"\n切分後的訊息：{words_str}"
+
+    
+          
+            
+    
+
+          
+          Expand Down
+    
+    
+  
             
         # currency
         elif event.message.text.lower().startswith("$$$$"):
@@ -96,14 +98,11 @@ def handle_message(event):
             msg += currency.get_currency_spot("USD") 
             msg += "\n"
             msg += currency.get_currency_spot("EUR") 
-
         
         elif event.message.text.lower().startswith("$$"):
             msg = currency.get_currency(event.message.text.replace("$$", "", 1).strip())
-
     except Exception as e:
         msg = f"發生錯誤: {str(e)}"
-
     finally:
         line_bot_api.reply_message(
             event.reply_token,
@@ -126,7 +125,6 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text="我是時下流行的AI智能，目前可以為您服務囉，歡迎來跟我互動~"))
         return
-
     if event.message.text.replace("%%", "", 1) == "安靜":
         working_status = False
         line_bot_api.reply_message(
@@ -141,7 +139,5 @@ def handle_message(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=reply_msg))
-
-
 if __name__ == "__main__":
     app.run()
