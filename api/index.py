9e435aff71443fe57a5460bf2a99defdc8eebf39
josecc92 <51939862+josecc92@users.noreply.github.com>
2024-06-13 10:22:33 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -53,10 +53,10 @@ def handle_message(event):
         if event.message.text.lower().startswith("version"):
             msg = "20240613"
             
-        if event.message.text.lower().startswith("denni$"):
+        elif event.message.text.lower().startswith("denni$"):
             msg = "你問的好，DENNI$是一位傳奇般的富豪，他的財富程度超越了大多數人的想像。他的財富來源非常多元化，涵蓋了房地產、科技、金融和創業等領域。\n\nDENNI$擁有一個私人島嶼，這座島嶼被他打造成一個真正的天堂。島上有一座宏偉的別墅，擁有無敵海景和無邊際游泳池。他的別墅內設施一應俱全，包括私人電影院、保齡球場、溫泉浴場和網球場。"
 
-        if event.message.text.lower().startswith("miles"):
+        elif event.message.text.lower().startswith("miles"):
             user_message = event.message.text.strip().lower()
             # 切分訊息為單詞列表
             words = user_message.split()
@@ -70,23 +70,21 @@ def handle_message(event):
                     msg = "提供的指令不正確，請使用以下格式：miles <購買里程數> <cash 或 spot> <獲得的里程百分比>"
             else:
                 msg = "提供的指令不完整或格式不正確，請使用以下格式：miles <購買里程數> <cash 或 spot> <獲得的里程百分比>"
-        else:
-            msg = "歡迎使用本服務！請輸入 'miles' 來查看相關指令。"
             
-        if event.message.text.lower().startswith("asa$$$$"):
+        elif event.message.text.lower().startswith("asa$$$$"):
             msg = "ASA里程計算器\n"
             msg += "請輸入購買里程數和獲得的里程百分比，例如：ASA$$$$1000 50\n"
 
 
         # currency
-        if event.message.text.lower().startswith("$$$$"):
+        elif event.message.text.lower().startswith("$$$$"):
             msg = currency.get_currency("JPY") 
             msg += "\n"
             msg += currency.get_currency("USD") 
             msg += "\n"
             msg += currency.get_currency("EUR") 
         
-        if event.message.text.lower().startswith("$$n$$"):
+        elif event.message.text.lower().startswith("$$n$$"):
             msg = currency.get_currency_spot("JPY") 
             msg += "\n"
             msg += currency.get_currency_spot("USD") 
@@ -94,7 +92,7 @@ def handle_message(event):
             msg += currency.get_currency_spot("EUR") 
 
         
-        if event.message.text.lower().startswith("$$"):
+        elif event.message.text.lower().startswith("$$"):
             msg = currency.get_currency(event.message.text.replace("$$", "", 1).strip())
 
     except Exception as e:
